d8770c42f0dc78dc63a7372f2dbec5e208daee72
arsrna <root@arsrna.cn>
2025-10-09 15:31:35 +0800

    feat(index): 完成时间线功能

--- example/App.tsx
+++ example/App.tsx
@@ -1,12 +1,35 @@
+import { SyntheticEvent, useEffect, useRef, useState } from 'react';
 import Component from '../packages/Component';
 import './index.scss'
-import 'tdesign-react/dist/tdesign.min.css'
+import { characters, data, order } from './data';
 
+const items = data.map(m => ({
+  time: m.t, content: m.c
+}));
+
+const characterItems = order.map(m => ({
+  time: m.time,
+  content: m.characters.length === characters.length ? "合唱" : m.characters.map(c => characters[c].cv).join(' ')
+}))
 
 function App() {
+  const audio = useRef(null);
+  const [currentTime, setCurrentTime] = useState(0);
+  function ontimeupdate() {
+    setCurrentTime(audio.current.currentTime + 0.3);
+    requestAnimationFrame(ontimeupdate);
+  }
+
+  useEffect(() => {
+    if (audio.current === null) return;
+    ontimeupdate()
+  }, [])
+
   return (
     <>
-      <Component />
+      <Component items={items} currentTime={currentTime} scale={10} totalTime={audio.current?.duration} />
+      <Component items={characterItems} currentTime={currentTime} scale={5} totalTime={audio.current?.duration} />
+      <audio src='/index.mp3' controls ref={audio} style={{ width: '100%' }} />
     </>
   );
 }
--- packages/Component.tsx
+++ packages/Component.tsx
@@ -1,3 +1,42 @@
-export default function Component(){
-    return <h1>Powered by Ar-Sr-Na</h1>
+import { CSSProperties, HTMLAttributes, ReactNode, useRef } from 'react'
+import style from './index.module.scss'
+import { getDuration, randomRGBColor } from './utils'
+export interface itemsType {
+    time: number,
+    style?: CSSProperties
+    content?: ReactNode
+}
+
+export default function Component({
+    items, currentTime, totalTime, scale = 1, indicator
+}: {
+    items: itemsType[],
+    currentTime: number
+    totalTime: number,
+    scale?: number
+    indicator?: HTMLAttributes<HTMLDivElement>
+}) {
+    const colors = useRef(new Array(items.length).fill(0).map(randomRGBColor))
+
+    return <div className={style.timeline}>
+        <div className={style['timeline-indicator']} {...indicator}></div>
+        <div className={style['timeline-items']}
+            style={{
+                '--translate': `${scale * 50 / scale - ((currentTime / totalTime) * scale * 100)}%`
+            } as CSSProperties}
+        >
+            {items.map((item, index) => {
+                const timeArray = items.map(item => item.time);
+                const itemDuration = getDuration(timeArray, index);
+                const width = (itemDuration / totalTime) * scale * 100 + '%';
+
+                return <span className={style['timeline-item']} style={{
+                    '--bg': item.style?.background || colors.current[index],
+                    width,
+                } as CSSProperties} >
+                    {item.content}
+                </span>
+            })}
+        </div>
+    </div>
 }
